1e4475bffd1999b267dcf1a1b68f843b8c9c7acb
oberoii <oberoiiji@gmail.com>
2026-05-30 17:45:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
-const express=require('express'); const app=express(); app.get('/',(req,res)=>res.send('API Running')); app.listen(3000);
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.json({
+    success: true,
+    message: "WhatsApp Dashboard API Running"
+  });
+});
+
+module.exports = app;
